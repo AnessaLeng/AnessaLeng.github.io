932f6f777c486cc08098e43ce26e65747fbd1e05
Anessa Leng <ane.leng955@gmail.com>
2023-10-03 16:27:51 -0400

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -5,7 +5,7 @@ function scriptTest()
 
 function username() 
 {
-    var username = firstscripts.html.getElementById(name);
+    var username = firstscripts.html.getElementById(username);
     return username;
 }
 
@@ -17,7 +17,8 @@ function emotion()
 
 function popUp() 
 {
-    alert("Amber Lion welcomes you, " + username() + "!\nWe're glad you are doing " + emotion() + ".");
+    var companyName = "Leng Inc.";
+    alert(companyName + " welcomes you, " + username() + "!\nWe're glad you are doing " + emotion() + ".");
 }
 
 function shape() 
@@ -25,34 +26,37 @@ function shape()
     var number = firstscripts.html.getElementById(number);
     var shape;
 
-    if (number >= 10) {
-        shape = "decagon";
+    if (number > 10.99) {
+        shape = "big shape";
     }
-    else if (number > 9.00 && number < 9.99) {
+    else if (number >= 10.00 && number <= 10.99) {
+        shape = "decagon"
+    }
+    else if (number >= 9.00 && number <= 9.99) {
         shape = "enneagon"
     }
-    else if (number > 8.00 && number < 8.99) {
+    else if (number >= 8.00 && number <= 8.99) {
         shape = "octagon"
     }
-    else if (number > 7.00 && number < 7.99) {
+    else if (number >= 7.00 && number <= 7.99) {
         shape = "heptagon"
     }
-    else if (number > 6.00 && number < 6.99) {
+    else if (number >= 6.00 && number <= 6.99) {
         shape = "hexagon"
     }
-    else if (number > 5.00 && number < 5.99) {
+    else if (number >= 5.00 && number <= 5.99) {
         shape = "pentagon"
     }
-    else if (number > 4.00 && number < 4.99) {
+    else if (number >= 4.00 && number <= 4.99) {
         shape = "tetragon"
     }
-    else if (number > 3.00 && number < 3.99) {
+    else if (number >= 3.00 && number <= 3.99) {
         shape = "trigon"
     }
-    else if (number > 2.00 && number < 2.99) {
+    else if (number >= 2.00 && number <= 2.99) {
         shape = "digon"
     }
-    else if (number > 1.00 && number < 1.99) {
+    else if (number >= 1.00 && number <= 1.99) {
         shape = "henagon"
     }
     else {
@@ -61,9 +65,13 @@ function shape()
     alert(shape);
 }
 
-function 
+function sum()
 {
-
+    var number1 = firstscripts.html.getElementById(number1);
+    var number2 = firstscripts.html.getElementById(number2);
+    
+    var sum = number1 + number2;
+    return sum;
 }
 
 function 
