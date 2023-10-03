d9e3a3e3508024673e5eae5c872c097536de805c
Anessa Leng <ane.leng955@gmail.com>
2023-10-03 16:16:25 -0400

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,3 +1,82 @@
-function scriptTest() {
+function scriptTest() 
+{
     alert("Hey my script is running");
+}
+
+function username() 
+{
+    var username = firstscripts.html.getElementById(name);
+    return username;
+}
+
+function emotion() 
+{
+    var emotion = firstscripts.html.getElementById(emotion);
+    return emotion;
+}
+
+function popUp() 
+{
+    alert("Amber Lion welcomes you, " + username() + "!\nWe're glad you are doing " + emotion() + ".");
+}
+
+function shape() 
+{
+    var number = firstscripts.html.getElementById(number);
+    var shape;
+
+    if (number >= 10) {
+        shape = "decagon";
+    }
+    else if (number > 9.00 && number < 9.99) {
+        shape = "enneagon"
+    }
+    else if (number > 8.00 && number < 8.99) {
+        shape = "octagon"
+    }
+    else if (number > 7.00 && number < 7.99) {
+        shape = "heptagon"
+    }
+    else if (number > 6.00 && number < 6.99) {
+        shape = "hexagon"
+    }
+    else if (number > 5.00 && number < 5.99) {
+        shape = "pentagon"
+    }
+    else if (number > 4.00 && number < 4.99) {
+        shape = "tetragon"
+    }
+    else if (number > 3.00 && number < 3.99) {
+        shape = "trigon"
+    }
+    else if (number > 2.00 && number < 2.99) {
+        shape = "digon"
+    }
+    else if (number > 1.00 && number < 1.99) {
+        shape = "henagon"
+    }
+    else {
+        shape = "circle"
+    }
+    alert(shape);
+}
+
+function 
+{
+
+}
+
+function 
+{
+    
+}
+
+function 
+{
+    
+}
+
+function 
+{
+    
 }
