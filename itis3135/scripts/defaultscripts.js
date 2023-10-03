8d79feed18ca86ddd2c2dd06cee4f98136cde2cd
Anessa Leng <ane.leng955@gmail.com>
2023-10-03 19:15:23 -0400

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -3,68 +3,6 @@ function scriptTest()
     alert("Hey my script is running");
 }
 
-function username() 
-{
-    var username = firstscripts.html.getElementById(username);
-    return username;
-}
-
-function emotion() 
-{
-    var emotion = firstscripts.html.getElementById(emotion);
-    return emotion;
-}
-
-function popUp() 
-{
-    var companyName = "Leng Inc.";
-    alert(companyName + " welcomes you, " + username() + "!\nWe're glad you are doing " + emotion() + ".");
-}
-
-function shape() 
-{
-    var number = firstscripts.html.getElementById(number);
-    var shape;
-
-    if (number > 10.99) {
-        shape = "big shape";
-    }
-    else if (number >= 10.00 && number <= 10.99) {
-        shape = "decagon"
-    }
-    else if (number >= 9.00 && number <= 9.99) {
-        shape = "enneagon"
-    }
-    else if (number >= 8.00 && number <= 8.99) {
-        shape = "octagon"
-    }
-    else if (number >= 7.00 && number <= 7.99) {
-        shape = "heptagon"
-    }
-    else if (number >= 6.00 && number <= 6.99) {
-        shape = "hexagon"
-    }
-    else if (number >= 5.00 && number <= 5.99) {
-        shape = "pentagon"
-    }
-    else if (number >= 4.00 && number <= 4.99) {
-        shape = "tetragon"
-    }
-    else if (number >= 3.00 && number <= 3.99) {
-        shape = "trigon"
-    }
-    else if (number >= 2.00 && number <= 2.99) {
-        shape = "digon"
-    }
-    else if (number >= 1.00 && number <= 1.99) {
-        shape = "henagon"
-    }
-    else {
-        shape = "circle"
-    }
-    alert(shape);
-}
-
 function sum()
 {
     var number1 = firstscripts.html.getElementById(number1);
@@ -88,7 +26,3 @@ function
 {
     
 }
-
-const input = firstscripts.html.createElement('input');
-input.setAttribute('type', 'text');
-input.setAttribute('id', 'username');
