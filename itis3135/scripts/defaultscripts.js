5dcf267a609034bdbac0055bc3fcb0521c80f679
Anessa Leng <ane.leng955@gmail.com>
2023-10-03 20:07:36 -0400

    update

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,27 +2,3 @@ function scriptTest()
 {
     alert("Hey my script is running");
 }
-
-function sum()
-{
-    var number1 = firstscripts.html.getElementById(number1);
-    var number2 = firstscripts.html.getElementById(number2);
-    
-    var sum = number1 + number2;
-    return sum;
-}
-
-function 
-{
-    
-}
-
-function 
-{
-    
-}
-
-function 
-{
-    
-}
