5404d34840764c300b73def85747e9ea4ba890ba
Anessa Leng <ane.leng955@gmail.com>
2023-10-04 19:31:36 -0400

    update

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -12,17 +12,47 @@ function validateFields() {
 }
 
 function reset() {
-
+    
 }
 
 function addCourse() {
-    var course = byo_introduction.getElementById();
+    var count = 1;
+    var li = document.createElement("li");
+    var input = document.createElement("input");
+    li.setAttribute("value", count)
+    input.setAttribute("type", "text");
+
+    if (count == 1)
+    {
+        input.textContent = "ITSC2181 - Intro to Computer Systems: It is required for my degree.";
+    }
+    else if (count == 2) 
+    {
+        input.textContent = "STAT2122 - Intro to Probability and Statistics: Unfortunately, I need this for my degree.";
+    }
+    else if (count == 3)
+    {
+        input.textContent = "ITSC3146 - Intro to Operating Systems and Networking: It is required for my degree.";
+    }
+    else if (count = 4)
+    {
+        input.textContent = "ITIS3135 - Web App Design and Development: It is required for my degree, but I am also thrilled to learn more about this subject.";
+    }
+    else 
+    {
+        input.textContent = "Course Number - Course Name: Description.";
+    }
+    count++;
+    document.body.appendChild(li);
+    document.body.appendChild(input);
 }
 
 function deleteCourse() {
-
+    var li = document.getElementById("value");
+    document.body.removeChild(value);
 }
 
 function submit() {
-    emptyFields();
+    validateFields();
+    document.write();
 }
