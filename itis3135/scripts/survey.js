81b64b43c6f29c8b9e09ea0ac98e23e8b085f10b
Anessa Leng <ane.leng955@gmail.com>
2023-10-24 18:47:22 -0400

    update

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -12,6 +12,7 @@ function loadImage() {
         var defaultimageURL = "images/intro-chinatown-ny.jpg";
         document.getElementById('preview').src = defaultimageURL;
     }
+    return imageURL;
 }
 
 function validateFields() {
@@ -35,13 +36,15 @@ function addCourses() {
     var textareaCourseID = document.createElement('textarea');
     var textareaCourseReason = document.createElement('textarea');
     textareaCourseID.setAttribute("type", "textarea");
+    textareaCourseID.setAttribute("id", "course-id");
     textareaCourseID.setAttribute("rows", "2");
     textareaCourseID.setAttribute("cols", "20");
-    textareaCourseID.setAttribute("value", "ITSC2181 - Intro to Computer Systems: ");
+    textareaCourseID.textContent = "ITSC2181 - Intro to Computer Systems";
     textareaCourseReason.setAttribute("type", "textarea");
+    textareaCourseReason.setAttribute("id", "course-reason");
     textareaCourseReason.setAttribute("rows", "2");
     textareaCourseReason.setAttribute("cols", "20");
-    textareaCourseReason.setAttribute("value", "It is required for my degree.");
+    textareaCourseReason.textContent = "It is required for my degree.";
     var form = document.getElementById('courses');
     li.appendChild(textareaCourseID)
     li.append(" : ");
@@ -55,54 +58,59 @@ function deleteCourse() {
 }
 
 function submit() {
-    if (!validateFields()) { return; }
+    if (!validateFields) { return; }
 
     var ul = document.createElement('ul');
     var username = document.getElementById('username').value;
     var mascot = document.getElementById('mascot').value;
-    
-    var image = document.getElementById('image').files[0];
-    if (image) {
-        var imageURL = URL.createObjectURL(image);
-        document.getElementById('intro-image').src = imageURL;
-    }
+    var image = loadImage();
     var image_caption = document.getElementById('image-caption').value;
     var personal_background = document.getElementById('personal-background').value;
     var professional_background = document.getElementById('professional-background').value;
     var academic_background = document.getElementById('academic-background').value;
     var background_in_web_development = document.getElementById('background-in-web-development').value;
     var platform = document.getElementById('platform').value;
-    
+        
     var course = {id: document.getElementById('course-id').value, description: document.getElementById('course-reason').value};
     courses.push(course);
 
     var funny_thing = document.getElementById('funny-thing').value;
     var anything_else = document.getElementById('anything-else').value;
 
-    document.getElementById('intro-username').textContent = username;
-    document.getElementById('intro-mascot').textContent = mascot;
-    document.getElementById('intro-image').textContent = image;
-    document.getElementById('intro-image-caption').textContent = image_caption;
-    document.getElementById('intro-personal-background').textContent = personal_background;
-    document.getElementById('intro-professional-background').textContent = professional_background;
-    document.getElementById('intro-academic-background').textContent = academic_background;
-    document.getElementById('intro-background-in-web-development').textContent = background_in_web_development;
-    document.getElementById('intro-platform').textContent = platform;
-    document.getElementById('intro-courses').textContent = course;
+    document.getElementById('intro-username').value = username;
+    document.getElementById('intro-mascot').value = mascot;
+    document.getElementById('intro-image').value = image;
+    document.getElementById('intro-image-caption').value = image_caption;
+    document.getElementById('intro-personal-background').value = personal_background;
+    document.getElementById('intro-professional-background').value = professional_background;
+    document.getElementById('intro-academic-background').value = academic_background;
+    document.getElementById('intro-background-in-web-development').value = background_in_web_development;
+    document.getElementById('intro-platform').value = platform;
+    document.getElementById('intro-courses').value = course;
     var ul2 = document.createElement('ul2');
     for (var i = 0; i < courses.length; i++) {
         var li = document.createElement('li');
-        li.textContent = courses[i].id + " : " + courses[i].description;
+        li.value = courses[i].id + " : " + courses[i].description;
         ul2.appendChild(li);
     }
+    document.getElementById('intro-funny-thing').value = funny_thing;
+    document.getElementById('intro-anything-else').value = anything_else;    
+}
 
-    document.getElementById('intro-funny-thing').textContent = funny_thing;
-    document.getElementById('intro-anything-else').textContent = anything_else;
-
+button.addEventListener('click', function() {
     document.getElementById('byo-introduction').style.display = 'none';
-    document.getElementById('byo-intro').style.display = 'block';    
+    document.getElementById('byo-intro').style.display = 'block';  
+});
 
+document.addEventListener('DOMContentLoaded', submit);
 
+var h2 = document.createElement('h2');
+h2.innerHTML = '<h2>BYO Introduction</h2>';
+document.body.appendChild(h2);
+var ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+    /*
     document.body.appendChild(ul);
     ul.appendChild(username);
     ul.appendChild(mascot);
@@ -117,4 +125,4 @@ function submit() {
     document.body.appendChild(ul2);
     ul.appendChild(funny_thing);
     ul.appendChild(anything_else);
-}
+    */
