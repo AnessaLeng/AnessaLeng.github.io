af678caaa7616e1056c3acf28e5d62be2936310a
Anessa Leng <ane.leng955@gmail.com>
2023-11-01 11:50:37 -0400

    update

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -91,6 +91,49 @@ function deleteCourse() {
     li.parentNode.removeChild(li);
 }
 
+let form = document.getElementById('byo-introduction');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!validateFields()) { return false; } 
+    
+    var username = document.getElementById('username').value;
+    var mascot = document.getElementById('mascot').value;
+    var image = document.getElementById('preview').src;
+    var image_caption = document.getElementById('image-caption').value;
+    var personal_background = document.getElementById('personal-background').value;
+    var professional_background = document.getElementById('professional-background').value;
+    var academic_background = document.getElementById('academic-background').value;
+    var background_in_web_development = document.getElementById('background-in-web-development').value;
+    var platform = document.getElementById('platform').value;
+    var course = {id: document.getElementById('course-id').value, description: document.getElementById('course-reason').value};
+    courses.push(course);
+    var funny_thing = document.getElementById('funny-thing').value;
+    var anything_else = document.getElementById('anything-else').value;
+    
+    var form = document.getElementById('byo-intro');
+    form.querySelector('#intro-username').childNodes[1].textContent = username;
+    form.querySelector('#intro-mascot').childNodes[1].textContent = mascot;
+    form.querySelector('#intro-image').src = image;
+    form.querySelector('#intro-image-caption').textContent = image_caption;
+    form.querySelector('#intro-personal-background').childNodes[1].textContent = personal_background;
+    form.querySelector('#intro-professional-background').childNodes[1].textContent = professional_background;
+    form.querySelector('#intro-academic-background').childNodes[1].textContent = academic_background;
+    form.querySelector('#intro-background-in-web-development').childNodes[1].textContent = background_in_web_development;
+    form.querySelector('#intro-platform').childNodes[1].textContent = platform;
+    
+    var courseList = form.querySelector('#intro-courses').querySelector('ul');
+    courseList.innerHTML = '';
+    for (var i=0; i < courses.length; i++) {
+        var courseItem = document.createElement('li');
+        courseItem.textContent = courses[i].id + ': ' + courses[i].description;
+        courseList.appendChild(courseItem);
+    }
+
+    form.querySelector('#intro-funny-thing').childNodes[1].textContent = funny_thing;
+    form.querySelector('#intro-anything-else').childNodes[1].textContent = anything_else;
+});
+/*
 function submit() {
     if (!validateFields()) { return false; } 
     
@@ -132,3 +175,4 @@ function submit() {
     
     return false;
 }
+*/
