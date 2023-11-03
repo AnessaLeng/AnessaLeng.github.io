c47aee0193195fd9110845058c698fa061999572
Anessa Leng <ane.leng955@gmail.com>
2023-11-02 23:26:39 -0400

    update

---
+++ itis3135/scripts/project.js
@@ -0,0 +1,18 @@
+let form = document.getElementById('company-name');
+
+window.onload = function() {
+    var buttons = [];
+    buttons.forEach(function(button) {
+        if(button == '') {
+
+        }
+    });
+}
+
+
+
+form.addEventListener('a', (e) => {
+    e.preventDefault();
+
+
+});
