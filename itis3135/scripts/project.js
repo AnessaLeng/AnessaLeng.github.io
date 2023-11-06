2dc85325d965905d4bf5457cd02710eca2a1f09c
Anessa Leng <ane.leng955@gmail.com>
2023-11-05 23:59:15 -0500

    update

--- itis3135/scripts/project.js
+++ itis3135/scripts/project.js
@@ -1,18 +1,20 @@
 let form = document.getElementById('company-name');
 
 window.onload = function() {
-    var buttons = [];
+    var buttons = ['click-to-reserve'];
     buttons.forEach(function(button) {
-        if(button == '') {
-
+        if(button == 'click-to-reserve') {
+            document.getElementById(button).addEventListener('click', reservation);
         }
     });
 }
-
-
 
 form.addEventListener('a', (e) => {
     e.preventDefault();
 
 
 });
+
+function reservation() {
+
+}
