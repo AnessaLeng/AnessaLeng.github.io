3bce9054ab30d56dca34484aa85581593f258e56
Anessa Leng <ane.leng955@gmail.com>
2023-11-08 18:49:51 -0500

    Update fizz_buzz_bang.js

--- itis3135/scripts/fizz_buzz_bang.js
+++ itis3135/scripts/fizz_buzz_bang.js
@@ -1,3 +1,34 @@
 const fizzBuzzParagraph = document.getElementById('fizzbuzzcontent');
-let text = document.createTextNode('Hello');
-fizzBuzzParagraph.appendChild(text);
+fizzBuzzParagraph.innerHTML = fizzBuzz().toString();
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        let print = i;
+        if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+            print = 'fizzbuzzBANG';
+        }
+        else if (i % 3 === 0 && i % 5 === 0) {
+            print = 'fizzbuzz';
+        }
+        else if (i % 5 === 0 && i % 7 === 0) {
+            print = 'buzzBANG';
+        }
+        else if (i % 3 === 0 && i % 7 === 0) {
+            print = 'fizzBANG';
+        }
+        else if (i % 3 === 0) {
+            print = 'fizz';
+        }
+        else if (i % 5 === 0) {
+            print = 'buzz';
+        }
+        else if (i % 7 === 0) {
+            print = 'BANG';
+        }
+        else {
+            print = i;
+        }
+        fizzBuzzParagraph.innerHTML += print + ' ';
+    }
+}
+fizzBuzz();
